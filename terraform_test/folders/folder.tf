138c738b77951f239a7c063e83dc37b0e1000be1
Vikeltor <115746129+Vikeltor@users.noreply.github.com>
2025-03-14 16:14:57 +0300

    Delete terraform_test directory

--- terraform_test/folders/folder.tf
+++
@@ -1,8 +0,0 @@
-resource "yandex_resourcemanager_cloud" "my_cloud" {}
-
-resource "yandex_resourcemanager_folder" "my_folder" {
-  cloud_id    = "yandex_resourcemanager_cloud.my_cloud.id"
-  name        = "diplom"
-  description = "Проект дипломной работы"
-}
-
