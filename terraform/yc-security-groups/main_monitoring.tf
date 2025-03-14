651d880821ffcfbf18a48dcd9c7435e3f46cd817
Vikeltor <Vikeltor2@gmail.com>
2025-03-14 12:30:27 +0000

    first commit

---
+++ terraform/yc-security-groups/main_monitoring.tf
@@ -0,0 +1,70 @@
+resource "yandex_compute_instance" "vm_prometheus" {
+  name                      = "vm-prometheus"
+  allow_stopping_for_update = true
+  platform_id               = "standard-v1"
+  zone                      = "ru-central1-a"
+  hostname                  = "debian-vm-prometheus"
+
+  resources {
+    cores          = 2
+    memory         = 2
+    core_fraction  = 20
+  }
+
+  boot_disk {
+    disk_id     = "${yandex_compute_disk.disk_prometheus.id}"
+  }
+  
+  network_interface {
+    subnet_id   = "${yandex_vpc_subnet.subnet_a.id}"
+    ip_address  = "10.128.0.21"
+    nat         = false
+    security_group_ids  = ["${yandex_vpc_security_group.sg_prometheus.id}"]
+  }
+  
+  scheduling_policy {
+    preemptible = true
+  }
+
+
+  metadata = {
+    ssh-keys    = "yc-user:${file("~/.ssh/id_ed25519.pub")}"
+    user-data   = "#cloud-config\ndatasource:\n Ec2:\n  strict_id: false\nssh_pwauth: no\nusers:\n- name: yc-user\n  sudo: ALL=(ALL) NOPASSWD:ALL\n  shell: /bin/bash\n  ssh_authorized_keys:\n  - ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPni+n1QcIIto6Us+Rq+8Eu+KRp73uGoDhZigSvgvCxw vikeltor@compute-vm-2-2-10-hdd-1735043319359"
+  }
+}
+
+resource "yandex_compute_instance" "vm_grafana" {
+  name                      = "vm-grafana"
+  allow_stopping_for_update = true
+  platform_id               = "standard-v1"
+  zone                      = "ru-central1-a"
+  hostname                  = "debian-vm-grafana"
+
+  resources {
+    cores          = 2
+    memory         = 2
+    core_fraction  = 20
+  }
+
+  boot_disk {
+    disk_id     = "${yandex_compute_disk.disk_grafana.id}"
+  }
+  
+  
+  network_interface {
+    subnet_id   = "${yandex_vpc_subnet.subnet_a.id}"
+    ip_address  = "10.128.0.22"
+    nat         = true
+    security_group_ids  = ["${yandex_vpc_security_group.sg_grafana.id}"]
+  }
+  
+  scheduling_policy {
+    preemptible = true
+  }
+
+
+  metadata = {
+    ssh-keys    = "yc-user:${file("~/.ssh/id_ed25519.pub")}"
+    user-data   = "#cloud-config\ndatasource:\n Ec2:\n  strict_id: false\nssh_pwauth: no\nusers:\n- name: yc-user\n  sudo: ALL=(ALL) NOPASSWD:ALL\n  shell: /bin/bash\n  ssh_authorized_keys:\n  - ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPni+n1QcIIto6Us+Rq+8Eu+KRp73uGoDhZigSvgvCxw vikeltor@compute-vm-2-2-10-hdd-1735043319359"
+  }
+}
